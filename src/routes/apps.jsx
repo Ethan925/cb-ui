ee51af7c50a7754fb5e7fdc7ce0b79916fba9074
Ethan Long <ethan.long@vertical-knowledge.com>
2022-05-10 19:35:19 -0400

    basic react router setup

---
+++ src/routes/apps.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Apps extends React.Component {
+  componentDidMount() {
+    console.log("hello");
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Apps test</h2>
+      </div>
+    )
+  }
+}
+export default Apps
---
+++ src/routes/plans.jsx
@@ -0,0 +1,7 @@
+export default function Plans() {
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h2>Plans</h2>
+    </main>
+  );
+}
